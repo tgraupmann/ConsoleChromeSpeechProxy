49d208784cb165891934470dc31953296476fd77
Tim Graupmann <tgraupmann@gmail.com>
2017-05-06 07:48:39 -0700

    Initial commit of project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleChromeSpeechProxy
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            HttpListener yeah = new HttpListener();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
